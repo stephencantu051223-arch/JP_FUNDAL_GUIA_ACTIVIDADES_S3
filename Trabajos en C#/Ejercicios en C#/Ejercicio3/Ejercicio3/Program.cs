dba38ade21d20d330d5defcccda8e7bc16aff06b
elpaiba <benjaminpaiba54@gmail.com>
2026-04-14 01:54:06 -0500

    subiendo carpeta con ejercicios en C#

---
+++ Trabajos en C#/Ejercicios en C#/Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class Program
+    {
+        //Conversor de temperatura (°C → °F y °K)
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese temperatura en °C: ");
+            double celsius = double.Parse(Console.ReadLine());
+
+            // Fórmula Fahrenheit: F = C × (9.0/5) + 32
+            double fahrenheit = celsius * (9.0 / 5) + 32;
+
+            // Fórmula Kelvin: K = C + 273.15
+            double kelvin = celsius + 273.15;
+
+            Console.WriteLine($"Temperatura en Celsius: {celsius} °C");
+            Console.WriteLine($"Convertida a Fahrenheit: {fahrenheit:F2} °F");
+            Console.WriteLine($"Convertida a Kelvin: {kelvin:F2} °K");
+        }
+    }
+}
---
+++ Trabajos en C#/Ejercicios en C#/Ejercicios1/Ejercicio1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Número de habitaciones: ");
+            int habitaciones = int.Parse(Console.ReadLine());
+
+            Console.Write("Número de baños: ");
+            int baños = int.Parse(Console.ReadLine());
+
+            Console.Write("Número de patios: ");
+            int patios = int.Parse(Console.ReadLine());
+
+            // Cada habitación cuesta S/.30.00
+            int costoHabitaciones = habitaciones * 30;
+
+            // Cada baño cuesta S/.10.00
+            int costoBaños = baños * 10;
+
+            // Cada patio cuesta S/.15.00
+            int costoPatios = patios * 15;
+
+            // Total = habitaciones*30 + baños*10 + patios*15
+            int total = costoHabitaciones + costoBaños + costoPatios;
+
+            Console.WriteLine($"Habitaciones: {habitaciones} → S/.{costoHabitaciones}");
+            Console.WriteLine($"Baños: {baños} → S/.{costoBaños}");
+            Console.WriteLine($"Patios: {patios} → S/.{costoPatios}");
+            Console.WriteLine($"Total mensual: S/.{total}"); 
+        }
+    }
+}
